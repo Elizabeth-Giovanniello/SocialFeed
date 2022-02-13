dd3e5fac6b4b01138dafeb4e8835d47f41a95ad0
Lizzy <ea.giovanniello@gmail.com>
2022-02-13 13:00:32 -0500

    Successfully imported bootstrap icons and created like and dislike buttons with thumbs-up and thumbs-down icons.

--- src/Components/DisplayPosts/DisplayPosts.jsx
+++ src/Components/DisplayPosts/DisplayPosts.jsx
@@ -4,10 +4,11 @@ const DisplayPosts = (props) => {
     return (
         props.posts.map((post) => {
             return (
-              <div className="container">
+              <div className="container border rounded">
                 <h5>{post.posterName}</h5>
                 <p>{post.content}</p>
                 <button class="bi bi-hand-thumbs-up"></button>
+                <button class="bi bi-hand-thumbs-down"></button>
               </div>
             )
         })
